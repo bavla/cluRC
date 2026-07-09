6848aebb76984aa127f796e3fde818c02d3ef1a6
bavla <vladimir.batagelj@gmail.com>
2026-07-09 15:48:23 +0200

    Add files via upload

--- igraph/cluRC.R
+++ igraph/cluRC.R
@@ -13,22 +13,22 @@ varCutree <- function(R,var,vmin,vmax){
   n <- length(var); part <- rep(999999,n); nclust <- 0
   value <- cbind(var,rep(0,n))
   for(i in 1:(n-1)){
-    j <- R$merge[i,1]; if(j==0) next 
+    j <- R$merge[i,1]; if(j==0) next
     a <- ifelse(j<0,value[-j,1],value[j,2])
-    j <- R$merge[i,2]; if(j==0) next 
+    j <- R$merge[i,2]; if(j==0) next
     b <- ifelse(j<0,value[-j,1],value[j,2])
     value[i,2] <- a+b
   }
   value[n,2] <- 0
   for(i in 1:(n-1)){
     if(value[i,2]<=vmax) next
-    l <- R$merge[i,1]; r <- R$merge[i,2] 
+    l <- R$merge[i,1]; r <- R$merge[i,2]
     if(l==0) a <- 0 else a <- ifelse(l<0,value[-l,1],value[l,2])
     if(r==0) b <- 0 else b <- ifelse(r<0,value[-r,1],value[r,2])
     if(min(a,b)>vmax) next
     if(a<=vmax) if(a>=vmin) {nclust <- nclust+1; mark(l,nclust)} else mark(l,0)
-    if(b<=vmax) if(b>=vmin) {nclust <- nclust+1; mark(r,nclust)} else mark(r,0)    
-  }  
+    if(b<=vmax) if(b>=vmin) {nclust <- nclust+1; mark(r,nclust)} else mark(r,0)
+  }
   return(list(part=part,value=value))
 }
 
@@ -77,7 +77,7 @@ cluRCdist <- function(N,D,method="max",strategy="tolerant"){
   idx <- function(i,j) {if (i<j) return(n*(i-1) - i*(i-1)/2 + j-i) else
     return(n*(j-1) - j*(j-1)/2 + i-j)}
   if(strategy %in% c("tolerant", "leader", "strict")){
-    tol <- strategy=="tolerant"; nos <- strategy!="strict"} else 
+    tol <- strategy=="tolerant"; nos <- strategy!="strict"} else
     {cat("*** Error - Unknown strategy:", strategy,"\n"); return(NULL)}
   cat("Clustering with relational constraint based on the class dist\n")
   cat("by Vladimir Batagelj, March 2018 / July 2026\n")
@@ -88,9 +88,9 @@ cluRCdist <- function(N,D,method="max",strategy="tolerant"){
   n <- numL <- attr(D,"Size"); numLm <- numL-1
   Ro <- neighborhood(N,order=1,mode="out",V(N))
   Ri <- neighborhood(N,order=1,mode="in",V(N))
-  for(i in 1:length(Ro)) { li <- length(Ro[[i]]) 
+  for(i in 1:length(Ro)) { li <- length(Ro[[i]])
     if(li<=1) Ro[[i]] <- 0 else Ro[[i]] <- as.vector(Ro[[i]][2:li]) }
-  for(i in 1:length(Ri)) { li <- length(Ri[[i]]) 
+  for(i in 1:length(Ri)) { li <- length(Ri[[i]])
     if(li<=1) Ri[[i]] <- 0 else Ri[[i]] <- as.vector(Ri[[i]][2:li]) }
   active <- 1:numL; m <- matrix(nrow=numLm,ncol=2)
   node <- rep(0,numL); h <- numeric(numLm); w <- rep(1,numL)
@@ -98,14 +98,14 @@ cluRCdist <- function(N,D,method="max",strategy="tolerant"){
   # determine the closest pair of clusters (p,q)
     nn <- length(active); ind <- rep(Inf,nn); dd <- rep(Inf,nn)
     for(a in seq_along(active)) {i <- active[a]
-    #  if((length(Ro[[i]])==1)&&(Ro[[i]][1]==0)) dd[a] <- Inf else      
+    #  if((length(Ro[[i]])==1)&&(Ro[[i]][1]==0)) dd[a] <- Inf else
       for(j in Ro[[i]]) if((j>0)&&(i!=j)) if(D[idx(i,j)] < dd[[a]]) {
         dd[[a]] <- D[idx(i,j)]; ind[[a]] <- j} }
     pq <- which.min(dd)
     if((length(pq)==0)|is.null(pq)) break
     dpq <- dd[[pq]]
   # join the closest pair of clusters
-    p<-active[pq]; q <- ind[pq]; 
+    p<-active[pq]; q <- ind[pq];
     if(is.infinite(q)){
       cat("several components\n")
       dpq <- h[k-1]*1.1; p <- active[1]
@@ -113,22 +113,22 @@ cluRCdist <- function(N,D,method="max",strategy="tolerant"){
         if(node[p]==0) m[k,1] <- -p else m[k,1] <- node[p]
         if(node[q]==0) m[k,2] <- -q else m[k,2] <- node[q]
         w[p] <- w[q]+w[p]; node[[k]] <- k; h[k] <- dpq
-        p <- k; k <- k+1 
+        p <- k; k <- k+1
       }
       break
     }
-    h[k] <- dpq 
+    h[k] <- dpq
     if(node[p]==0) m[k,1] <- -p else m[k,1] <- node[p]
     if(node[q]==0) m[k,2] <- -q else m[k,2] <- node[q]
     active <- setdiff(active,q)
     # cat("active =",active,"\n")
-    Rop <- setdiff(Ro[[p]],q); Rip <- setdiff(Ri[[p]],q) 
+    Rop <- setdiff(Ro[[p]],q); Rip <- setdiff(Ri[[p]],q)
     Roq <- setdiff(Ro[[q]],p); Riq <- setdiff(Ri[[q]],p)
     # cat("Rop:",Rop,"  Rip:",Rip,"\n")
     for(s in Riq) if(s>0) Ro[[s]] <- setdiff(Ro[[s]],q)
     r <- p; Ror <- 0; Rir <- Rip
     if(tol) Ror <- Rop else
-      for(s in Rop) if(s>0) Ri[[s]] <- setdiff(Ri[[s]],p) 
+      for(s in Rop) if(s>0) Ri[[s]] <- setdiff(Ri[[s]],p)
     Ror <- union(Ror,Roq)
     # cat("Ror:",Ror,"\n")
     for(s in Roq) if(s>0) Ri[[s]] <- setdiff(union(Ri[[s]],r),q)
@@ -155,7 +155,7 @@ cluRCdist <- function(N,D,method="max",strategy="tolerant"){
 }
 
 # Clustering with relational constraint based on a network
-cluRCnet <- function(N,method="max",strategy="tolerant"){
+cluRCnet <- function(N,method="max",strategy="tolerant",step=0){
   orDendro <- function(i){if(i<0) return(-i)
     return(c(orDendro(m[i,1]),orDendro(m[i,2])))}
   key <- function(i,j)
@@ -166,7 +166,7 @@ cluRCnet <- function(N,method="max",strategy="tolerant"){
   cat("Clustering with relational constraint based on a dictionary\n")
   cat("by Vladimir Batagelj, March 2018 / July 2026\n")
   cat("Method:",method,"  Strategy:",strategy,"\n")
-  print(paste("Started:",Sys.time()))
+  print(paste("Started:",Sys.time())); flush.console()
 # network -> dict
   hD <- new.env(); n <- vcount(N); np <- n+1
   for(i in 1:ecount(N)) {e <- ends(N,i,names=FALSE)[1,]
@@ -183,6 +183,7 @@ cluRCnet <- function(N,method="max",strategy="tolerant"){
   active <- 1:numL; m <- matrix(nrow=numLm,ncol=2)
   node <- rep(0,numL); h <- numeric(numLm); w <- rep(1,numL)
   for(k in 1:numLm){
+    if(step>0) if(k %% step==0) {cat(date()," n =",k,"\n"); flush.console()}
   # determine the closest pair of clusters (p,q)
     nn <- length(active); ind <- rep(Inf,nn); dd <- rep(Inf,nn)
     for(a in seq_along(active)) {i <- active[a]
